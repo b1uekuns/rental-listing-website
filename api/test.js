9b92cc42b7429a15a1f316f3f911e57c97332be0
b1uekuns <come.here.pro@gmail.com>
2025-08-04 17:25:39 +0700

    up

--- api/test.js
+++ api/test.js
@@ -1,9 +1,9 @@
 // Simple test for Vercel
 module.exports = (req, res) => {
-  res.json({ 
-    message: "Hello from Vercel!", 
+  res.json({
+    message: "Hello from Vercel!",
     timestamp: new Date().toISOString(),
     method: req.method,
-    url: req.url
+    url: req.url,
   });
 };
